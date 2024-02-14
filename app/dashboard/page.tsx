b2e6636b2c18ed5be4001b7c5aaede6928bfeccf
johannfroesa <johann@baetter.io>
2024-02-14 12:56:43 +0100

    new data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,14 @@
 import BarChart from "../components/BarChart";
 import LineChart from "../components/LineChart";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { promises as fs } from 'fs';
 
 
-export default function DashboardPage(){
+export default async function DashboardPage(){
+    const file = await fs.readFile(process.cwd() + '/app/randomdata/randomdatatest.json', 'utf8');
+    const data = JSON.parse(file);
+    console.log(data);
+
     return(
         <div className="grid items-start gap-8">
             <h1 className="text-2xl md:text-2xl">Dashboard</h1>
@@ -15,7 +20,7 @@ export default function DashboardPage(){
                                 <CardDescription>Top Data.</CardDescription>
                             </CardHeader>
                             <CardContent>
-                            <BarChart/>
+                            <BarChart data={data}/>
                             </CardContent>
                     </Card>
                     </div>
@@ -26,7 +31,7 @@ export default function DashboardPage(){
                                 <CardDescription>Top Data.</CardDescription>
                             </CardHeader>
                             <CardContent>
-                            <LineChart/>
+                            <LineChart data={data}/>
                             </CardContent>
                     </Card>
                     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import { unstable_noStore as noStore } from "next/cache";
 
 
 export default async function Home() {
+
   noStore();
   const { isAuthenticated } = getKindeServerSession ();
 
